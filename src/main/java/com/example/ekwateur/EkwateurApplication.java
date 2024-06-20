d638d050b7cf3a97e6f098c1b8baf90c7eb0ede2
bao <bao-trn@outlook.fr>
2024-06-20 20:00:00 +0200

    init

---
+++ src/main/java/com/example/ekwateur/EkwateurApplication.java
@@ -0,0 +1,13 @@
+package com.example.ekwateur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EkwateurApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EkwateurApplication.class, args);
+    }
+
+}
